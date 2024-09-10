6a2e1469938064168658d4c2096552e2cd926973
Arefun Naher Nipa <arefunnnipa@gmail.com>
2024-09-10 21:42:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//Function
+let x = '/* Function Count */'
+console.log(x);
+
+function flowers(){
+   let rose = 10
+   let tulip = 5
+   let z = rose+tulip
+   let result = z
+   console.log(result);
+   
+}
+flowers()
+
+//array with loop
+let xx = '/* Array with loop */'
+console.log(xx);
+
+let colorNam = ['Red', 'Green', 'Blue', 'Orange', 'Yellow', 'Aash']
+
+for (let i = 0; i < colorNam.length; i++) {
+    console.log(colorNam [i]);
+    
+}
+
+
+//Speech Synthesis
+let y = '/* Speech Synthesis */'
+console.log(y);
+
+let utterance = new SpeechSynthesisUtterance("Hello, This is Nipa");
+speechSynthesis.speak(utterance);
+
+
+//Speech Synthesis with array/loop/funtion
+let yy = '/* Speech Synthesis with array/loop/funtion */'
+console.log(yy);
+
+let colorNamm = ['Red', 'Green', 'Blue', 'Orange', 'Yellow', 'Aash']
+function colorName(colors){
+    let utterance = new SpeechSynthesisUtterance(`${colors} color`);
+    speechSynthesis.speak(utterance);
+}
+
+for (let i = 0; i < colorNamm.length; i++) {
+    colorName(colorNamm [i]);
+    
+}
+
+
+
+
